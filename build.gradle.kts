a6450d19f498e974f5681837ef94c3584b153c47
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2024-03-14 21:40:42 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    java
+    id("xyz.jpenilla.run-paper") version "2.2.2"
+}
+
+repositories {
+    mavenCentral()
+
+    maven {
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+}
+
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+tasks {
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+
+        expand("version" to project.version)
+    }
+
+    runServer {
+        minecraftVersion("1.20.4")
+    }
+}
---
+++ src/main/java/dev/warriorrr/entityttl/EntityTTL.java
@@ -0,0 +1,66 @@
+package dev.warriorrr.entityttl;
+
+import com.destroystokyo.paper.event.entity.EntityAddToWorldEvent;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Registry;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Entity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EntityTTL extends JavaPlugin implements Listener {
+    private final Map<NamespacedKey, Long> lifetimes = new HashMap<>();
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        loadConfig();
+
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    public void loadConfig() {
+        final ConfigurationSection entities = getConfig().getConfigurationSection("entities");
+
+        for (final String entity : entities.getKeys(false)) {
+            final NamespacedKey key = NamespacedKey.fromString(entity);
+
+            if (key == null) {
+                getLogger().warning("'" + entity + "' is not a valid namespaced key.");
+                continue;
+            }
+
+            if (Registry.ENTITY_TYPE.get(key) == null) {
+                getLogger().warning(key + " is not a valid entity type.");
+                continue;
+            }
+
+            final long lifetime = entities.getLong(entity);
+            if (lifetime < 0)
+                continue;
+
+            lifetimes.put(key, lifetime);
+        }
+    }
+
+    @EventHandler
+    public void onEntityAddToWorld(final EntityAddToWorldEvent event) {
+        final Entity entity = event.getEntity();
+
+        final Long removeTicks = lifetimes.get(entity.getType().getKey());
+        if (removeTicks == null)
+            return;
+
+        // Remove immediately
+        if (removeTicks == 0) {
+            entity.getScheduler().run(this, task -> entity.remove(), () -> {});
+            return;
+        }
+
+        entity.getScheduler().runDelayed(this, task -> entity.remove(), () -> {}, removeTicks);
+    }
+}
